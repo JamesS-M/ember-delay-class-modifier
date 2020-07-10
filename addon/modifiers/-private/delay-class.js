ddc8e3ba4702501ad1550512af7fb2cf3dfe9ce4
James Summerby-Murray <jsummerb@gmail.com>
2020-07-10 17:39:46 -0300

    set default delay = 1000

--- addon/modifiers/-private/delay-class.js
+++ addon/modifiers/-private/delay-class.js
@@ -1,7 +1,7 @@
 import { later } from '@ember/runloop'
 import { assert } from '@ember/debug'
 
-export default function (element, [className, delay], method) {
+export default function (element, [className, delay = 1000], method) {
   assert(`The class name must be a valid string. You provided "${className}".`, className)
   assert(`The class name must be a string. You provided ${className}, which is of type "${typeof className}"`, typeof className === 'string')
   later(() => element.classList[method](className), delay)
